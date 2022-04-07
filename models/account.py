281b0b8ae4a1cc5214f359efd06c81aa48e756a5
jeffrepo <jeffer.leo93@gmail.com>
2022-04-06 21:54:36 -0600

    impuesto

--- models/account.py
+++ models/account.py
@@ -138,10 +138,12 @@ class AccountTax(models.Model):
             tax_amount = tax._compute_amount(base, price_unit, quantity, product, partner)
             if not round_tax:
                 tax_amount = round(tax_amount, prec)
-                logging.warning('TAX AMONUT ROUND')
-                logging.warning(tax_amount)
             else:
                 tax_amount = currency.round(tax_amount)
+
+            tax_amount = round(tax_amount, prec)
+            logging.warning('TAX AMONUT ROUND')
+            logging.warning(tax_amount)
 
             if price_include:
                 total_excluded -= tax_amount

ab3e1db79d9414ab56efa5f94d62b2622b0d3c53
jeffrepo <jeffer.leo93@gmail.com>
2022-04-06 21:42:43 -0600

    logg

--- models/account.py
+++ models/account.py
@@ -9,7 +9,7 @@ from odoo.tools.float_utils import float_round as round
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import UserError, ValidationError
 from odoo import api, fields, models, _
-
+import logging
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"

8a518486401b561ae8a3ce6f84dc20fd4deab776
jefferson Silva <8306339+jeffrepo@users.noreply.github.com>
2024-08-21 17:09:46 -0600

    Update account_move.py

--- models/account_move.py
+++ models/account_move.py
@@ -315,7 +315,7 @@ class AccountMove(models.Model):
             precio_unitario = linea.price_unit
             precio = linea.price_unit * linea.quantity
             descuento = ((linea.quantity * linea.price_unit) - linea.price_total) if linea.discount > 0 else 0
-            precio_subtotal = linea.price_subtotal
+            precio_subtotal = '{:.6f}'.format(linea.price_subtotal)
             TagCantidad = etree.SubElement(TagItem,DTE_NS+"Cantidad",{})
             TagCantidad.text = str(cantidad)
             TagUnidadMedida = etree.SubElement(TagItem,DTE_NS+"UnidadMedida",{})
